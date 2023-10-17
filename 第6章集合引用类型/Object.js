e488eeba1d4a0af27f71194ae9d75ffe2d301bd4
zongang_v <zongang05@chinasoftinc.com>
2023-10-17 14:28:15 +0800

    Object使用

--- 第6章集合引用类型/Object.js
+++ 第6章集合引用类型/Object.js
@@ -20,4 +20,23 @@ let person1 = {
 };
 console.log(person1);
 
+// 接下来指定了name属性，后跟一个冒号，然后是属性的值
+
+let person2 = {
+    "name":"李龙",
+    "age":29,
+    5:true
+};
+console.log(person2);
+// 这个例子会得到一个带有属性name、age和5的对象。注意，属性值会自动转换为字符串
+
+/*
+当然也可以用对象字面量表示法来定义一个只有默认属性和方法的对象，只要使用一对大括号，中间留空就行了。
+*/ 
+
+let person3 = { }; //与new Object相同
+person3.name = "纵昂";
+person3.age = 29;
+console.log(person3);
+
 
